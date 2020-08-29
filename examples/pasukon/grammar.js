988ca144cf4b78df4011be7fadf3f290546a1726
Federico Ramirez <fedra.arg@gmail.com>
2020-08-29 15:51:41 -0300

    Remove obsolete example

--- examples/pasukon/grammar.js
+++
@@ -1,8 +0,0 @@
-const Pasukon = require('../../lib/pasukon')
-const fs = require('fs')
-const path = require('path')
-
-const parser = new Pasukon(fs.readFileSync(path.join(__dirname, 'grammar.g')))
-console.log(parser.parse(`
-Hi
-`.trim()))
