5651f6673d9ce7625159f11a38701d3ff60e9883
Federico Ramirez <fedra.arg@gmail.com>
2020-09-01 18:39:02 -0300

    Typo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
   Token: require('./lib/lexer/token'),
   UnaryCombinator: require('./lib/parsers/combinators/unary-combinator'),
-  BinaryCombinator: require('./lib/parsers/combinators/unary-combinator'),
+  BinaryCombinator: require('./lib/parsers/combinators/binary-combinator'),
   Pasukon: require('./lib/pasukon')
 }
